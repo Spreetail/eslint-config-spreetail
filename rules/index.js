26ce9834c54d787cbb12197a1a7e56622fcc9852
Alex Bainter <alex.bainter@vminnovations.com>
2017-04-26 14:32:41 -0500

    Fix rules export.

--- rules/index.js
+++ rules/index.js
@@ -1,9 +1,9 @@
-var bestPractices = require('best-practices');
-var ecmascript6 = require('ecmascript-6');
-var possibleErrors = require('possible-errors');
-var strictMode = require('strict-mode');
-var stylisticIssues = require('stylistic-issues');
-var variables = require('variables');
+var bestPractices = require('./best-practices');
+var ecmascript6 = require('./ecmascript-6');
+var possibleErrors = require('./possible-errors');
+var strictMode = require('./strict-mode');
+var stylisticIssues = require('./stylistic-issues');
+var variables = require('./variables');
 
 var allRules = {};
 var hasOwn = Object.prototype.hasOwnProperty;
@@ -12,7 +12,7 @@ var hasOwn = Object.prototype.hasOwnProperty;
 forEach(function(rules) {
     for(var key in rules) {
         if (hasOwn.call(rules, key)) {
-            allRules[key] == rules[key];
+            allRules[key] = rules[key];
         }
     }
 });
