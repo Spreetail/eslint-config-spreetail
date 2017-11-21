6f6de0148c24fee4f106185291eb05cfebdbf32f
Alex Bainter <alex.bainter@vminnovations.com>
2017-11-21 16:26:21 -0600

    Fix reference to react/jsx-no-comment-textnodes.

--- rules/react/jsx.js
+++ rules/react/jsx.js
@@ -2,7 +2,7 @@ var rules = {
   'react/jsx-filename-extension': 'error',
   'react/jsx-key': 'error',
   'react/jsx-no-bind': 'error',
-  'react/jsx-no-comment-textnode': 'warn',
+  'react/jsx-no-comment-textnodes': 'warn',
   'react/jsx-no-duplicate-props': 'warn',
   'react/jsx-no-target-blank': 'error',
   'react/jsx-no-undef': 'error',
